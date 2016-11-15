920f7bfb538af51efade873567d859fd7d62b860
Guilherme Rambo <insidegui@gmail.com>
2016-11-15 20:23:44 -0200

    Initial commit

---
+++ IGListDiff/IGListDiff.h
@@ -0,0 +1,27 @@
+//
+//  IGListDiff.h
+//  IGListDiff
+//
+//  Created by Guilherme Rambo on 10/12/16.
+//  Copyright © 2016 Guilherme Rambo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for IGListDiff.
+FOUNDATION_EXPORT double IGListDiffVersionNumber;
+
+//! Project version string for IGListDiff.
+FOUNDATION_EXPORT const unsigned char IGListDiffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IGListDiff/PublicHeader.h>
+
+#import <IGListDiff/IGListDiffCore.h>
+#import <IGListDiff/IGListDiffable.h>
+#import <IGListDiff/IGListExperiments.h>
+#import <IGListDiff/IGListIndexPathResult.h>
+#import <IGListDiff/IGListIndexSetResult.h>
+#import <IGListDiff/IGListMacros.h>
+#import <IGListDiff/IGListMoveIndex.h>
+#import <IGListDiff/IGListMoveIndexPath.h>
+#import <IGListDiff/NSObject+IGListDiffable.h>
